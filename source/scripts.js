6d7c291780c7669c472de8f7fa1284741853d7af
annmiro <a.knyazevav@yandex.ru>
2023-06-17 19:37:48 +0400

    final corrections index adaptive done

--- source/scripts.js
+++ source/scripts.js
@@ -1,5 +1,6 @@
 const menuButton = document.querySelector(`.header__menu-button`);
 const menu = document.querySelector(`.header__nav-list`);
+const header = document.querySelector(`.header`);
 
 menuButton.addEventListener('click', (evt) => {
   evt.preventDefault()
@@ -7,11 +8,18 @@ menuButton.addEventListener('click', (evt) => {
   if (menu.classList.contains('header__nav-list--open')) {
     menu.classList.remove('header__nav-list--open')
     menu.classList.add('header__nav-list--close')
+    menuButton.classList.add('header__menu-button--burger')
+
+    setTimeout(() => {
+      header.classList.remove('header--mobile-menu')
+    }, 1000)
   } else {
+    menuButton.classList.remove('header__menu-button--burger')
     menu.classList.add('header__nav-list--open')
     menu.classList.remove('header__nav-list--close')
+    header.classList.add('header--mobile-menu')
   }
-  
+
 })
 
 console.log(menuButton)

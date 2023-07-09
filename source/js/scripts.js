a1768c18b66156b45adeed9116e4fdfdb962e0af
annmiro <a.knyazevav@yandex.ru>
2023-07-09 19:44:40 +0400

    range slider finalized

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,8 +1,8 @@
 const menuButton = document.querySelector(`.header__menu-button`);
 const menu = document.querySelector(`.header__nav-list`);
 const header = document.querySelector(`.header`);
-
-const range = document.querySelector(`.range-slider__custom`)
+const catFatFrame = document.querySelector(".example__image-frame--cat-fat");
+const range = document.querySelector(`.range-slider__custom`);
 
 menuButton.addEventListener('click', (evt) => {
   evt.preventDefault()
@@ -25,6 +25,7 @@ menuButton.addEventListener('click', (evt) => {
 })
 
 range.addEventListener(`input`, (evt) => {
+  catFatFrame.style.width = 100 - evt.target.value + "%";
   console.log(`change`, evt.target.value)
 })
 
